940298e068b1ebb94e1c932c6668995a21667b69
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2022-05-04 22:11:41 +0530

    Initial commit

---
+++ Sort_Array.py
@@ -0,0 +1,25 @@
+class SortArray:
+    def sort(arr):
+        low = 0
+        mid = 0
+        high = a.length - 1
+        while mid <= high:
+            if a[mid] == 0:
+                # Swap(arr,arr[low],arr[mid])
+                low += 1
+                high += 1
+            if a[mid] == 1:
+                mid += 1
+            if a[mid] == 2:
+                temp = arr[mid]
+                arr[mid] = arr[high]
+                arr[high] = temp
+                high -= 1
+
+
+
+
+
+
+
+
